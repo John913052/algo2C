78eed559d6533384c3ec062bbd979b5af30f983c
Sandeep Sahajrao <sandeepsahajrao98@gmail.com>
2023-01-03 08:47:43 +0000

    nav ready

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+// import { Container, Row } from 'react-bootstrap'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AlgoNav from './components /AlgoNav'
+// import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='appcontainer'>
+      <AlgoNav></AlgoNav>
+    </div>
+      
+ 
   )
 }
 
