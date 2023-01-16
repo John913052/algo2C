cf6cb181ee20e3c6ad36254b60846313b24364c0
Sandeep Sahajrao <107790439+sandeepsahajrao@users.noreply.github.com>
2023-01-16 10:48:20 +0000

    space remove from app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react'
 // import { Container, Row } from 'react-bootstrap'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AlgoNav from './components /AlgoNav'
-// import Algodiv from './components /Algodiv';
-// import Downside from './components /Downside';
-// import Second from './components /Second';
-// import Resize from './components /Resize';
-import Demo from './components /Demo';
-import Question from './components /question/Question';
-// import Navbar from './components /dumy/Navbar';
+import AlgoNav from './components/AlgoNav'
+// import Algodiv from './components/Algodiv';
+// import Downside from './components/Downside';
+// import Second from './components/Second';
+// import Resize from './components/Resize';
+import Demo from './components/Demo';
+import Question from './components/question/Question';
+// import Navbar from './components/dumy/Navbar';
 
-// import Promote from './components /Promote';
+// import Promote from './components/Promote';
 // import { Container } from 'react-bootstrap';
 // import 'react-reflex/style.css';
 
